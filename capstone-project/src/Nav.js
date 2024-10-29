c2d4d4d11e5ce4a09abe51f76820a607a7d22afe
b4gabe <gbhilyard@yahoo.com>
2024-10-28 18:30:37 -0700

    Created some components, and rendered a logo.

---
+++ capstone-project/src/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Logo from './images/Logo.svg';
+
+const Nav = () => {
+    return (
+        <img>src={Logo} alt="Logo" style={{height: '50px'}}</img>
+    )
+}
+
+export default Nav;
