cf4d1f17abad24eacbfdf0641d947cc23c89521c
b4gabe <gbhilyard@yahoo.com>
2024-12-02 18:04:20 -0800

    added an icon

--- capstone-project/src/Footer.js
+++ capstone-project/src/Footer.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import FooterImage from './images/restaurantFood.jpg';
+import "./Footer.css"
+import { FaInstagramSquare } from "react-icons/fa";
+
+
+
 const Footer = () => {
     return (
-        <footer style={FooterBody}>
+        <footer className="FooterBody"> 
             <div>
             <img src={FooterImage} alt="A plate of food in the footer" style={{height: '310px', marginRight:'50px'}}/>
             </div>
-            <div style={FooterColumns}>
-                <ul style={FooterLinks}>
+            <div className="FooterColumns">
+                <ul className="FooterLinks">
                     <li style={{marginBottom:'10px'}}>Footer Navigation</li>
                     <li><a href='Footer' style={{textDecoration: 'none', color: 'black'}}>Home</a></li>
                     <li><a href='Footer' style={{textDecoration: 'none', color: 'black'}}>About</a></li>
@@ -18,17 +23,17 @@ const Footer = () => {
                 </ul>
            
             
-                <ul style={FooterLinks}>
+                <ul className="FooterLinks">
                     <li style={{marginBottom:'10px'}}>Contact</li>
-                    <li><a href='Footer' style={{textDecoration: 'none', color: 'black'}}>Address</a></li>
-                    <li><a href='Footer' style={{textDecoration: 'none', color: 'black'}}>Phone Number</a></li>
-                    <li><a href='Footer' style={{textDecoration: 'none', color: 'black'}}>E-mail</a></li>
+                    <li>Address</li>
+                    <li>Phone Number</li>
+                    <li>E-mail</li>
                 </ul>
           
           
-                <ul style={FooterLinks}>
+                <ul className="FooterLinks">
                     <li style={{marginBottom:'10px'}}>Social Media:</li>
-                    <li><a href='Footer' style={{textDecoration: 'none', color: 'black'}}>Instagram</a></li>
+                    <li><a href='Footer' style={{textDecoration: 'none', color: 'black'}}><FaInstagramSquare /></a></li>
                     <li><a href='Footer' style={{textDecoration: 'none', color: 'black'}}>Facebook</a></li>
                     <li><a href='Footer' style={{textDecoration: 'none', color: 'black'}}>Twitter</a></li>
                 </ul>
@@ -37,23 +42,9 @@ const Footer = () => {
     )
 }
 
+
+
+
 export default Footer;
 
 
-const FooterBody = {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '702px',
-    backgroundColor: '#495E57',
-}
-
-const FooterColumns = {
-    display: 'flex',
-}
-
-const FooterLinks = {
-    listStyleType: 'none',
-    textDecoration: 'none',
-    color: 'white',
-}
