b799ff6b147738801db43ae3ec77672e20b9b80a
b4gabe <gbhilyard@yahoo.com>
2025-01-13 20:35:41 -0800

    getting main.css main.js app.js and app.css up to date in repository

--- capstone-project/src/App.js
+++ capstone-project/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Nav from './Nav.js'
 import Main from './Main.js'
 import Footer from './Footer.js'
+import Highlights from './Highlights.js'
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <>
       <Nav />
       <Main />
+      <Highlights />
       <Footer />
       </>
       
--- capstone-project/src/Main.js
+++ capstone-project/src/Main.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import './Main.css'
+import heroImg from './images/restaurant.jpg'
+
+
 
 const Main = () => {
+
+    const handleReservationClick = () => {
+        window.location.href = '/reservations';
+    }
+
     return (
-        <h1>Main</h1>
+        <div className="heroSection">
+        <div className="heroSectionText">
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            <br /> Praesent in libero aliquet, volutpat eros et, commodo quam.
+            <br /> Quisque in placerat lacus. Vestibulum sit amet egestas est. 
+            <br />Etiam a sollicitudin nisl, in vulputate felis. </p>
+        <button onClick={handleReservationClick} className='reservationButton'>
+            Reserve a table
+        </button>
+        </div>
+        <div className="heroSectionImg">
+            <img src={heroImg} 
+            alt="outdoor patio area" 
+            style={{height: 400, borderRadius: 10 }}/>
+        </div>
+        </div>
     )
 }
 
