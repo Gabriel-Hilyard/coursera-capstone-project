bf95a76ea75f7b607e6af1594bc2ac56e1f7e84b
b4gabe <gbhilyard@yahoo.com>
2025-01-13 18:19:59 -0800

    added table reservation button and began creating the highlights component

---
+++ capstone-project/src/Highlights.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Highlights.css'
+
+const Highlights = () => {
+    return (
+    <div className='titleButton'>
+        <h1>Specials</h1>
+    </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Highlights;
